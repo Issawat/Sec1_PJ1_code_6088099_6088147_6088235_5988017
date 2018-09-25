f3b8aa02f0cfa59fe44239f3ea98b4e7256990f5
issawat <actiondeveloper1998@gmail.com>
2018-09-25 10:35:56 +0700

    add effect to index and history

---
+++ assets/js/navbar.js
@@ -0,0 +1,14 @@
+window.onscroll = function () { scroller() };
+  function scroller() {
+    var header = document.getElementById("nav");
+    var homeBtn = document.getElementById("homeBtn");
+    var sticky = header.offsetTop;
+    console.log(header.offsetTop, window.pageYOffset)
+    if (window.pageYOffset > 195) {
+      header.classList.add("sticky");
+      homeBtn.innerText = 'MU RILCA'
+    } else if (window.pageYOffset <= 195) {
+      header.classList.remove("sticky");
+      homeBtn.innerText = 'HOME'
+    }
+  }
